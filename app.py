e82227209928714fc3c3fa451700f487d11d87ed
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-04-25 10:15:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.subheader("장소 추가하기")
 new_name = st.text_input("장소 이름")
 new_region = st.selectbox("지역", ["강릉", "속초", "춘천"])
 new_indoor = st.radio("실내 여부", ["실내", "실외"])
-new_budget = st.number_input("예산")
+new_budget = st.number_input("예산", min_value = 0)
 new_description = st.text_input("한줄 설명")
 
 if st.button("장소 추가"):
