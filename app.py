ba40818e6e0db2c0ce883c49190f64ea237e83fa
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-02 09:49:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ elif menu == "장소 추가":
             if exists:
                 st.warning("이미 그 장소가 있습니다")
             else:
-                st.session_state.placelist = add_place(st.session_state.placelist,new_name,new_region,new_indoor,new_budget,new_description)
+                st.session_state.placelist = add_place(st.session_state.placelist, new_name, new_region, new_indoor, new_budget, new_description)
                 st.success("장소가 추가되었습니다.")
         else:
             st.warning("장소 이름을 입력하세요")
