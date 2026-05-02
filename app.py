1d45e82cca2a5748518eded8becbf18b80cb818b
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-02 09:46:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ def add_place(data, name, region, indoor, budget, description):
 
 st.title("강원 청소년 생활 도우미")
 
+menu = st.radio("메뉴를 선택하세요", ["지역 추가", "장소 추가", "추천 보기"])
+
 if menu == "지역 추가":
     new_region = st.text_input("새 지역 입력")
 
