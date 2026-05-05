087e5eed8ffa8ef59725ffcb3d252179c2304482
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-05 10:25:19 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -33,4 +33,9 @@ else:
 region_count = df["지역"].value_counts()
 
 st.subheader("지역별 장소 개수")
-st.bar_chart(region_count, range_scale = 1)
+st.bar_chart(region_count)
+
+avg_score = df["평점"].value_counts()
+
+st.subheader("지역별 평점")
+st.bar_chart(avg_score)
