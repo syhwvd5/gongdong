8c48eff956334be76f2e0422f1fc7f0094507061
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-16 10:10:21 +0900

    Update pbl-funtion.py

--- pbl-funtion.py
+++ pbl-funtion.py
@@ -17,25 +17,6 @@ def show_data(df):
     st.subheader("전체 장소 데이터")
     st.dataframe(df)
 
-import streamlit as st
-import pandas as pd
-
-st.title("강원생활도우미앱 2.0")
-st.write("엑셀 파일을 업로드하여 조건에 맞는 장소를 추천받아보세요.")
-
-def load_data(uploaded_file):
-    df = pd.read_excel(uploaded_file)
-    return df
-
-uploaded_file = st.file_uploader(
-    "장소 데이터 엑셀 파일을 업로드하세요(파일 확장자: [.xlsx])",
-    type=["xlsx"]
-)
-
-def show_data(df):
-    st.subheader("전체 장소 데이터")
-    st.dataframe(df)
-
 def filter_places(df, selected_region, selected_budget):
     result = df[
         (df["지역"] == selected_region) &
