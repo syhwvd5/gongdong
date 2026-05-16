1a52b56ad29ed263216705fdee360491dc4f4347
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-16 10:20:17 +0900

    Update pbl-funtion.py

--- pbl-funtion.py
+++ pbl-funtion.py
@@ -4,14 +4,14 @@ import pandas as pd
 st.title("강원생활도우미앱 2.0")
 st.write("엑셀 파일을 업로드하여 조건에 맞는 장소를 추천받아보세요.")
 
-def load_data(uploaded_file):
-    df = pd.read_excel(uploaded_file)
-    return df
-
 uploaded_file = st.file_uploader(
     "장소 데이터 엑셀 파일을 업로드하세요(파일 확장자: [.xlsx])",
     type=["xlsx"]
 )
+
+def load_data(uploaded_file):
+    df = pd.read_excel(uploaded_file)
+    return df
 
 def show_data(df):
     st.subheader("전체 장소 데이터")
