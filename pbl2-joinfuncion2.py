e4860410cbd2dce5ba54458898036034ffb0c77a
syhwvd5 <84524572+syhwvd5@users.noreply.github.com>
2026-05-30 10:15:18 +0900

    Update pbl2-joinfuncion2.py

--- pbl2-joinfuncion2.py
+++ pbl2-joinfuncion2.py
@@ -89,13 +89,14 @@ def show_chart(df):
     st.bar_chart(chart_data)
 
 
-uploaded_file = st.file_uploader(
-    "엑셀 파일을 업로드하세요",
-    type=["xlsx"]
-)
+
 
 if uploaded_file is None:
     st.info("엑셀 파일을 업로드하면 데이터가 표시됩니다")
+    uploaded_file = st.file_uploader(
+    "엑셀 파일을 업로드하세요",
+    type=["xlsx"]
+    )
 
 
 if uploaded_file is not None:
